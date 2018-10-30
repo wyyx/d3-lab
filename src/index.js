3325edf64e3e39072a3237b053a932d0803126e7
wyyx <gnehcyx@163.com>
2018-10-30 15:39:17 +0800

    Bar chart 1 demo

--- src/index.js
+++ src/index.js
@@ -1,48 +1,19 @@
 import './styles.scss'
 import * as d3 from 'd3'
 
-// enter(add)
-let ps = d3
-	.select('body')
-	.selectAll('p')
-	.data([ 4, 8, 15, 16, 23, 42 ])
-	.enter()
-	.append('p')
-	.text(function(d) {
-		return 'I’m number ' + d + '!'
-	})
+var data = [ 4, 8, 15, 16, 23, 42 ]
 
-// update
-setTimeout(() => {
-	ps.data([ 111, 222, 333 ]).text(function(d) {
-		return 'I’m number ' + d + '!'
-	})
-}, 3000)
+let x = d3.scaleLinear().domain([ 0, d3.max(data) ]).range([ 0, 420 ])
 
-// transition
 d3
-	.selectAll('p')
-	.transition()
-	.duration(500)
-	.delay(function(d, i) {
-		return i * 500
+	.select('.chart')
+	.selectAll('div')
+	.data(data)
+	.enter()
+	.append('div')
+	.style('width', function(d) {
+		return x(d) + 'px'
 	})
-	.style('background-color', function(d) {
-		return 'lightgreen'
+	.text(function(d) {
+		return d
 	})
-
-// second enter(add)
-setTimeout(() => {
-	ps
-		.data([ 114, 118, 115, 116, 1123, 1142, 444, 555, 666 ])
-		.enter()
-		.append('p')
-		.text(function(d) {
-			return 'I’m number ' + d + '!'
-		})
-}, 6000)
-
-// remove
-setTimeout(() => {
-	ps.remove()
-}, 9000)
