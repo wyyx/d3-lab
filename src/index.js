f63bfe84eec458c2bfa5363147f50be3ef5f9b88
wyyx <gnehcyx@163.com>
2018-10-31 16:02:16 +0800

    Encoding Ordinal Data

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ import * as d3 from 'd3'
 import './d3.tsv'
 
 d3.tsv('./d3.tsv').then(function(data) {
-	console.log('data', data)
-
 	data.forEach(function(d) {
 		d.value = +d.value
 	})
 
+	console.log('data', data)
+
 	var width = 960,
 		height = 500
+
+	var x = d3
+		.scaleBand()
+		.domain(
+			data.map(function(d) {
+				return d.name
+			})
+		)
+		.range([ 0, width ])
+		.padding(0.1)
 
 	var y = d3
 		.scaleLinear()
@@ -24,10 +34,8 @@ d3.tsv('./d3.tsv').then(function(data) {
 
 	var chart = d3.select('.chart').attr('width', width).attr('height', height)
 
-	var barWidth = width / data.length
-
-	var bar = chart.selectAll('g').data(data).enter().append('g').attr('transform', function(d, i) {
-		return 'translate(' + i * barWidth + ',0)'
+	var bar = chart.selectAll('g').data(data).enter().append('g').attr('transform', function(d) {
+		return 'translate(' + x(d.name) + ',0)'
 	})
 
 	bar
@@ -38,11 +46,11 @@ d3.tsv('./d3.tsv').then(function(data) {
 		.attr('height', function(d) {
 			return y(d.value)
 		})
-		.attr('width', barWidth - 1)
+		.attr('width', x.bandwidth)
 
 	bar
 		.append('text')
-		.attr('x', barWidth / 2)
+		.attr('x', x.bandwidth() / 2)
 		.attr('y', function(d) {
 			return height - y(d.value) + 3
 		})
