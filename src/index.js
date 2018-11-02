d72ee703249206dfa36382da33b0cea555ae851c
wyyx <gnehcyx@163.com>
2018-11-02 19:27:09 +0800

    Nesting and Index

--- src/index.js
+++ src/index.js
@@ -1,63 +1,9 @@
 import './styles.scss'
 import * as d3 from 'd3'
 
-var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+var td = d3.selectAll('tbody tr').selectAll('td')
 
-var svg = d3.select('svg'),
-	width = +svg.attr('width'),
-	height = +svg.attr('height'),
-	g = svg.append('g').attr('transform', 'translate(32,' + height / 2 + ')')
-
-function update(data) {
-	var t = d3.transition().duration(750)
-
-	// JOIN new data with old elements.
-	var text = g.selectAll('text').data(data, function(d) {
-		return d
-	})
-
-	// EXIT old elements not present in new data.
-	text
-		.exit()
-		.attr('class', 'exit')
-		.transition(t)
-		.attr('y', 60)
-		.style('fill-opacity', 1e-6)
-		.remove()
-
-	// UPDATE old elements present in new data.
-	text
-		.attr('class', 'update')
-		.attr('y', 0)
-		.style('fill-opacity', 1)
-		.transition(t)
-		.attr('x', function(d, i) {
-			return i * 32
-		})
-
-	// ENTER new elements present in new data.
-	text
-		.enter()
-		.append('text')
-		.attr('class', 'enter')
-		.attr('dy', '.35em')
-		.attr('y', -60)
-		.attr('x', function(d, i) {
-			return i * 32
-		})
-		.style('fill-opacity', 1e-6)
-		.text(function(d) {
-			return d
-		})
-		.transition(t)
-		.attr('y', 0)
-		.style('fill-opacity', 1)
-}
-
-// The initial display.
-update(alphabet)
-
-// Grab a random sample of letters from the alphabet, in alphabetical order.
-d3.interval(function() {
-	update(d3.shuffle(alphabet).slice(0, Math.floor(Math.random() * 26)).sort())
-}, 1500)
+td.style('color', function(d, i) {
+	console.log('index', i)
+	return i ? null : 'red'
+})
