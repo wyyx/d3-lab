8fd4779a7658ce3ea44ca38c56867d0b554dcc10
wyyx <gnehcyx@163.com>
2018-11-03 09:23:17 +0800

    Nesting and the Parent Node

--- src/index.js
+++ src/index.js
@@ -2,18 +2,13 @@ import './styles.scss'
 import * as d3 from 'd3'
 
 var matrix = [ [ 0, 1, 2, 3 ], [ 4, 5, 6, 7 ], [ 8, 9, 10, 11 ], [ 12, 13, 14, 15 ] ]
+var theadData = [ 'A', 'B', 'C', 'D' ]
 
-var tr = d3.selectAll('tbody tr').data(matrix)
+var table = d3.select('body').append('table')
 
-var td = tr
-	.selectAll('td')
-	.data(function(d, i) {
-		console.log('d', d)
-		console.log('i', i)
-		// matrix[i]
-		return d
-	})
-	.text(function(d) {
-		// matrix[i][i]
-		return d
-	})
+var thead = table.append('thead')
+thead.selectAll('th').data(theadData).enter().append('th').text(d => d)
+
+var tbody = table.append('tbody')
+var tr = tbody.selectAll('tr').data(matrix).enter().append('tr')
+var td = tr.selectAll('td').data(d => d).enter().append('td').text(d => d)
