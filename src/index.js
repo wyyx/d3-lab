9c2b6d8de2c042cdf55fc2fc76233bef5731e137
wyyx <gnehcyx@163.com>
2018-11-05 17:23:04 +0800

    Applying D3 Transitions

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import * as d3 from 'd3'
 
 //  the data that powers the bar chart, a simple array of numeric values
 // prettier-ignore
-var chartdata = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 
+var chartdata1 = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120,
   135, 150, 165, 180, 200, 220, 240, 270, 300, 330, 370, 410];
+
+// prettier-ignore
+var chartdata2 = [410, 370, 330, 270, 240, 220, 200, 180, 165, 150, 135, 130,
+    135, 150, 165, 180, 200, 220, 240, 270, 300, 330, 370, 410];
+
+var chartdata = chartdata1
 
 //  the size of the overall svg element
 var height = 200,
@@ -21,33 +27,83 @@ var color = d3
 	.range([ '#d6e9c6', '#bce8f1', '#faebcc', '#ebccd1' ])
 
 var dynamicColor
-d3
+
+var svg = d3
 	.select('#bar-chart')
 	.append('svg')
 	.attr('width', width)
 	.attr('height', height)
 	.style('background', '#dff0d8')
-	.selectAll('rect')
-	.data(chartdata)
-	.enter()
-	.append('rect')
-	.style('fill', (data, i) => color(i))
-	.style('stroke', '#31708f')
-	.style('stroke-width', '5')
-	.attr('width', x.bandwidth())
-	.attr('height', function(data) {
-		return y(data)
-	})
-	.attr('x', function(data, i) {
-		return x(i)
-	})
-	.attr('y', function(data) {
-		return height - y(data)
-	})
-	.on('mouseover', function(data) {
-		dynamicColor = this.style.fill
-		d3.select(this).style('fill', '#3c763d')
-	})
-	.on('mouseout', function(data) {
-		d3.select(this).style('fill', dynamicColor)
-	})
+
+function updateChart(data) {
+	// JOIN new data with old elements.
+	var chart = svg.selectAll('rect').data(data)
+
+	// EXIT old elements not present in new data.
+	chart.exit().remove()
+
+	// UPDATE old elements present in new data.
+	chart
+		.attr('height', 0)
+		.attr('y', height)
+		.transition()
+		.attr('height', function(data) {
+			return y(data)
+		})
+		.attr('y', function(data) {
+			return height - y(data)
+		})
+		.delay(function(data, i) {
+			return i * 20
+		})
+		.duration(1000)
+		.ease(d3.easeExpOut)
+
+	// ENTER new elements present in new data.
+	chart
+		.enter()
+		.append('rect')
+		.style('fill', (data, i) => color(i))
+		.style('stroke', '#31708f')
+		.style('stroke-width', '5')
+		.attr('width', x.bandwidth())
+		.attr('x', function(data, i) {
+			return x(i)
+		})
+		.attr('height', 0)
+		.attr('y', height)
+		.on('mouseover', function(data) {
+			dynamicColor = this.style.fill
+			d3.select(this).style('fill', '#3c763d')
+		})
+		.on('mouseout', function(data) {
+			d3.select(this).style('fill', dynamicColor)
+		})
+		.transition()
+		.attr('height', function(data) {
+			return y(data)
+		})
+		.attr('y', function(data) {
+			return height - y(data)
+		})
+		.delay(function(data, i) {
+			return i * 20
+		})
+		.duration(1000)
+		.ease(d3.easeExpOut)
+}
+
+updateChart(chartdata)
+
+// Change data
+var isChartata1 = true
+d3.select('#btn').on('click', () => {
+	if (isChartata1) {
+		chartdata = chartdata2
+	} else {
+		chartdata = chartdata1
+	}
+
+	isChartata1 = !isChartata1
+	updateChart(chartdata)
+})
