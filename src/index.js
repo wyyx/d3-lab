bf2267d72499df58b3e2fe30c18bf49f4d49420e
wyyx <gnehcyx@163.com>
2018-11-05 12:08:38 +0800

    Using Colors With Linear Scaling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import './styles.scss'
 import * as d3 from 'd3'
 
 //  the data that powers the bar chart, a simple array of numeric values
-var chartdata = [ 40, 60, 80, 100, 70, 120, 100, 60, 70, 150, 120, 140 ]
+// prettier-ignore
+var chartdata = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 
+  135, 150, 165, 180, 200, 220, 240, 270, 300, 330, 370, 410];
 
 //  the size of the overall svg element
 var height = 200,
@@ -11,8 +13,12 @@ var height = 200,
 	barWidth = 40,
 	barOffset = 20
 
-var x = d3.scaleBand().domain(d3.range(0, chartdata.length)).range([ 0, 720 ]).padding(0.2)
+var x = d3.scaleBand().domain(d3.range(0, chartdata.length)).range([ 0, 720 ])
 var y = d3.scaleLinear().domain([ 0, d3.max(chartdata) ]).range([ 0, 200 ])
+var color = d3
+	.scaleLinear()
+	.domain([ 0, chartdata.length * 0.33, chartdata.length * 0.66, chartdata.length ])
+	.range([ '#d6e9c6', '#bce8f1', '#faebcc', '#ebccd1' ])
 
 d3
 	.select('#bar-chart')
@@ -24,8 +30,8 @@ d3
 	.data(chartdata)
 	.enter()
 	.append('rect')
-	.style('fill', '#3c763d')
-	.style('stroke', '#d6e9c6')
+	.style('fill', (data, i) => color(i))
+	.style('stroke', '#31708f')
 	.style('stroke-width', '5')
 	.attr('width', x.bandwidth())
 	.attr('height', function(data) {
