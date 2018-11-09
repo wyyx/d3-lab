54ffc7841b9c19b88e1ef2574b3048cdf755f429
wyyx <gnehcyx@163.com>
2018-11-09 10:03:23 +0800

    eliminate the wiggle

--- src/index.js
+++ src/index.js
@@ -10,11 +10,9 @@ var svg = d3.select('svg'),
 	width = +svg.attr('width') - margin.left - margin.right,
 	height = +svg.attr('height') - margin.top - margin.bottom,
 	g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-
 var x = d3.scaleLinear().domain([ 0, n - 1 ]).range([ 0, width ])
 var y = d3.scaleLinear().domain([ -1, 1 ]).range([ height, 0 ])
 
-// Line generator
 var line = d3
 	.line()
 	.x(function(d, i) {
@@ -25,6 +23,14 @@ var line = d3
 	})
 
 g
+	.append('defs')
+	.append('clipPath')
+	.attr('id', 'clip')
+	.append('rect')
+	.attr('width', width)
+	.attr('height', height)
+
+g
 	.append('g')
 	.attr('class', 'axis axis--x')
 	.attr('transform', 'translate(0,' + y(0) + ')')
@@ -33,10 +39,11 @@ g
 g.append('g').attr('class', 'axis axis--y').call(d3.axisLeft(y))
 
 g
+	.append('g')
+	.attr('clip-path', 'url(#clip)')
 	.append('path')
 	.datum(data)
 	.attr('class', 'line')
-	.attr('d', line)
 	.transition()
 	.duration(500)
 	.ease(d3.easeLinear)
@@ -45,8 +52,10 @@ g
 function tick() {
 	// Push a new data point onto the back.
 	data.push(random())
+	// Redraw the line.
+	d3.select(this).attr('d', line).attr('transform', null)
+	// Slide it to the left.
+	d3.active(this).attr('transform', 'translate(' + x(-1) + ',0)').transition().on('start', tick)
 	// Pop the old data point off the front.
 	data.shift()
-	// Redraw the line (with the wrong interpolation).
-	d3.active(this).attr('d', line).transition().on('start', tick)
 }
